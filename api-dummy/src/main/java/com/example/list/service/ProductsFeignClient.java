736b941c71fe9e55ff2a788bd0807de421032516
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-02 22:45:05 -0300

    refactored

--- api-dummy/src/main/java/com/example/list/service/ProductsFeignClient.java
+++ api-dummy/src/main/java/com/example/list/service/ProductsFeignClient.java
@@ -6,10 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
-@FeignClient( name = "Dummy-Api",url = "https://dummyjson.com/products")
+@FeignClient(name = "Dummy-Api", url = "https://dummyjson.com/products")
 public interface ProductsFeignClient {
 
     @GetMapping("{id}")
