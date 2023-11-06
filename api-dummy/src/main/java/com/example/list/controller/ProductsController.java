92e57d3001fd8f3d5c6b4c72ba9b85a977f12b31
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-06 15:40:34 -0300

    fix: refactored

--- api-dummy/src/main/java/com/example/list/controller/ProductsController.java
+++ api-dummy/src/main/java/com/example/list/controller/ProductsController.java
@@ -2,13 +2,14 @@ package com.example.list.controller;
 
 import com.example.list.model.ProductsList;
 import com.example.list.service.ProductsFeignClient;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.list.model.Products;
 
 @RestController
-@RequestMapping("products")
+@RequestMapping("api/products")
 
 @CrossOrigin(origins = "http://localhost:4200/")
 public class ProductsController {
@@ -25,5 +26,4 @@ public class ProductsController {
     public ResponseEntity<ProductsList> getAllProducts() {
         return ResponseEntity.ok(productsFeignClient.getAllProducts());
     }
-
 }
